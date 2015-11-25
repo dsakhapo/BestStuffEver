8119704dcc8c8dd4178ed0266658fbd8af071fed
Dsakhapo <dsakhapo@gmail.com>
2015-11-24 22:15:03 -0800

    Actual Linked List Code

---
+++ src/com/darius/linkedlist/Node.java
@@ -0,0 +1,38 @@
+package com.darius.linkedlist;
+
+public class Node 
+{
+	private Node link;
+	private int data;
+	
+	public Node()
+	{
+		link = null;
+	}
+	
+	public Node(int d, Node n)
+	{
+		link = n;
+		data = d;
+	}
+	
+	public void setData(int d)
+	{
+		data = d;
+	}
+	
+	public int getData()
+	{
+		return data;
+	}
+	
+	public void setLink(Node n)
+	{
+		link = n;
+	}
+	
+	public Node getLink()
+	{
+		return link;
+	}
+}
